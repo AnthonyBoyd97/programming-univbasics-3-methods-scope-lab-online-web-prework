07c3eb40aa298eae3aa61b23f5ff032771cad0a0
Anthony N Boyd <anboyd97@gmail.com>
2019-09-04 06:10:15 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,12 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  link_doesnt_say_this = "It's Dangerous To Go Alone! Take This."
+  puts link_doesnt_say_this
+end
+
+def any_phrase (phrase)
+  puts phrase  
+end
